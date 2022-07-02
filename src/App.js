ee4f078a0249c9dcbc926da2e8a54912f933aef6
abhinavsiwal <abhinavsiwal@gmail.com>
2022-07-02 13:26:51 +0530

    Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "./index.css";
-import Home from "./components/Home";
+import Home from "./components/Home/Home";
 import TopNavbar from "./components/Navbar/TopNavbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BottomNavbar from "./components/Navbar/BottomNavbar";
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,39 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pry: "#AAAAAA",
+        "n-black": "#0B0B0B",
+        "n-bg-gray": "#181818",
+        "n-white": "#FDFDFD",
+        "n-orange": "#FF8A00",
+        "n-blue": "#0D3AFF",
+        "n-gray": "#555555",
+        "n-dark-blue": "#072D5B",
+        "n-dark-orange": "#AB5E03",
+        "n-light-green": "#23DF8C",
+        "n-dark-red": "#961A3D",
+        "n-hover": "rgba(24,24,24,2)",
+        "n-hover3": "#808080",
+        "n-hover2": "#666",
+      },
+      fontSize: {
+        "9px": "9px",
+        xxs: "0.625rem",
+        "11px": "0.688rem",
+        "13px": "0.8125rem",
+        "15px": "0.938rem",
+        "22px": "1.375rem",
+        "32px": "2rem ",
+      },
+      padding: {
+        2.5: "0.625rem",
+      },
+      margin: {
+        2.5: "0.625rem",
+      },
+    },
   },
   plugins: [],
 }
