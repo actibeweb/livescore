25e6a566e80569afb4bef8c04b63ee4956367da8
abhinavsiwal <abhinavsiwal@gmail.com>
2022-07-21 17:49:49 +0530

    tennis

--- src/api/tennis.js
+++ src/api/tennis.js
@@ -1,39 +1,28 @@
 import axios from "axios";
 
-export const getFixtures = async (date) => {
-  const options = {
-    method: "GET",
-    url: `https://tennis-live-data.p.rapidapi.com/matches-by-date/${date}`,
-    headers: {
-      "X-RapidAPI-Key": "97583ea2d3mshbbb0eaad4ab4058p12dad2jsn6981e3b54499",
-      "X-RapidAPI-Host": "tennis-live-data.p.rapidapi.com",
-    },
-  };
+export const getFixtures = async (formData) => {
   try {
-    const { data } = await axios(options);
+    const { data } = await axios.post(
+      `${process.env.REACT_APP_API_URL}/match/getByFilter/tennis`,
+      formData
+    );
     console.log(data);
     return data;
   } catch (err) {
-    console.log(err);
-    return err;
+    console.log(err);     
+    throw new err();
   }
 };
 
 export const getFixturesById = async (id) => {
-  const options = {
-    method: "GET",
-    url: `https://tennis-live-data.p.rapidapi.com/match/${id}`,
-    headers: {
-      "X-RapidAPI-Key": "97583ea2d3mshbbb0eaad4ab4058p12dad2jsn6981e3b54499",
-      "X-RapidAPI-Host": "tennis-live-data.p.rapidapi.com",
-    },
-  };
+ 
+
   try {
-    const { data } = await axios(options);
+    const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/match/get/${id}`);
     console.log(data);
     return data;
   } catch (err) {
     console.log(err);
-    return err;
+    throw new err();
   }
 };
--- src/components/GameDetails/TennisDetails.js
+++ src/components/GameDetails/TennisDetails.js
@@ -21,9 +21,9 @@ const TennisDetails = () => {
       setLoading(true);
       const data = await getFixturesById(id);
       console.log(data);
-      console.log(data.results);
-      setGame(data.results.match);
-      setTournament(data.results.tournament);
+      
+      setGame(data);
+      setTournament(data);
 
       setLoading(false);
     } catch (err) {
@@ -56,94 +56,48 @@ const TennisDetails = () => {
         <Loader />
       ) : (
         <>
-          {game && tournament && (
+          {game && (
             <div className="text-n-white py-1 lg:w-[50%] lg:mx-auto">
               <div className="px-2.5 mb-2 flex justify-between items-center">
                 <div className="flex items-center gap-2 mb-2">
+                 
                   <div className="grid">
-                    <p className="capitalize text-sm font-bold">{game.title}</p>
+                    <p className="capitalize text-sm font-bold">
+                      {game.competition}
+                    </p>
                     <p className="capitalize text-11px text-pry">
-                      {tournament.name +
-                        ", " +
-                        tournament.city +
-                        ", " +
-                        tournament.country}
+                      {game.season}
                     </p>
                   </div>
                 </div>
+           
               </div>
-              <div className="mx- 2.5 h-20 py-3 relative flex justify-center items-center bg-n-bg-gray rounded-lg">
-                {game.status !== "finished" ? (
-                  <div className="flex justify-center items-center w-10 mr-10">
-                    <div className="absolute top-[5px] bottom-[5px] left-0 rounded-tr-xl rounded-br-xl w-1 bg-n-orange"></div>
-                    {game.status === "notstarted" && (
-                      <p className="absolute left-5 text-14px text-center font-thin text-n-orange">
-                        {formatTime(game.date)}
+              <div className="mx-2.5 h-20 py-3 relative flex justify-center items-center bg-n-bg-gray rounded-lg">
+              <div className="grid gap-[6px] mr-10 w-10">
+              <p className="text-11px text-center font-thin text-n-orange">
+                        {game.time}
                       </p>
-                    )}
-                    {game.status === "inprogress" && (
-                      <p className="absolute left-5 text-14px text-center font-thin text-n-orange">
-                        Live
-                      </p>
-                    )}
                   </div>
-                ) : (
-                  <div className="mr-10 flex justify-center items-center ">
-                    <p className="text-14px text-center font-thin">
-                      {game.status}
-                    </p>
-                  </div>
-                )}
+
                 <div className="flex justify-between items-center gap-10 w-64">
                   <div className="flex flex-col justify-center gap-3">
-                    <div>
-                      <span className="text-18px text-center">
-                        {game.home.full_name}
-                      </span>
-                      {game.result &&
-                        game.result.winner_id === game.home_id && (
-                          <span> (W)</span>
-                        )}
-                    </div>
-                    <div>
-                      <span className="text-18px text-center">
-                        {game.away.full_name}
-                      </span>
-                      {game.result &&
-                        game.result.winner_id === game.away_id && (
-                          <span> (W)</span>
-                        )}
-                    </div>
+                 
+                    <p className="text-11px text-center">
+                      {game.home}
+                    </p>
+                  </div>
+                  <div>
+                    <p className="text-[22px] flex items-center gap-1 font-bold">
+                      <span> {"0"}</span> -{" "}
+                      <span> {"0"}</span>
+                    </p>
                   </div>
                   <div className="flex flex-col justify-center gap-3">
-                    <div className="flex gap-3">
-                      <span className="text-13px text-center">
-                        {game.result && game.result.home_set1}
-                      </span>
-                      <span className="text-13px text-center">
-                        {game.result && game.result.home_set2}
-                      </span>
-                      <span className="text-13px text-center">
-                        {game.result && game.result.home_set3}
-                      </span>
-                      <span className="text-13px text-center">
-                        {game.result && game.result.home_set4}
-                      </span>
-                    </div>
-                    <div className="flex gap-3">
-                      <span className="text-13px text-center">
-                        {game.result && game.result.away_set1}
-                      </span>
-                      <span className="text-13px text-center">
-                        {game.result && game.result.away_set2}
-                      </span>
-                      <span className="text-13px text-center">
-                        {game.result && game.result.away_set3}
-                      </span>
-                      <span className="text-13px text-center">
-                        {game.result && game.result.away_set4}
-                      </span>
-                    </div>
+                  
+
+                    <p className="text-11px text-center">
+                      {game.away}
+                    </p>
                   </div>
                 </div>
               </div>
@@ -182,41 +136,39 @@ const TennisDetails = () => {
                       </div>
                     </div>
                     <div className="px-2 py-3 flex gap-3 border-n-bg-gray border-b">
-                      <i className="fa fa-whistle"></i>
+                      <i className="fa fa-calendar"></i>
                       <div>
                         <p className="capitalize text-11px">
-                          {tournament.surface}
+                          {game.referee}
                         </p>
-                        <p className="capitalize text-9px">Surface</p>
+                        <p className="capitalize text-9px">Referee</p>
+                      </div>
+                    </div>
+                    <div className="px-2 py-3 flex gap-3 border-n-bg-gray border-b">
+                      <i className="fa fa-calendar"></i>
+                      <div>
+                        <p className="capitalize text-11px">
+                          {game.venue}
+                        </p>
+                        <p className="capitalize text-9px">Venue</p>
                       </div>
                     </div>
                     <div className="px-2 py-3 flex gap-3 border-n-bg-gray border-b">
                       <i className="fa fa-whistle"></i>
-                      <div>
-                        <p className="capitalize text-11px">{game.court}</p>
-                        <p className="capitalize text-9px">Court</p>
-                      </div>
-                    </div>
-                    <div className="px-2 py-3 flex gap-3">
-                      <i className="fa fa-stadium"></i>
-                      <div>
-                        <p className="capitalize text-11px">
-                          {tournament.city + ", " + tournament.country}
-                        </p>
-                        <p className="capitalize text-9px">venue</p>
-                      </div>
                     </div>
                   </div>
                 </div>
               )}
-              {activeTab === "Scorecard" && (
+              {/* {activeTab === "Scorecard" && (
                 <div className="mx-2.5 text-pry">
                   <div className="mt-3 border-n-bg-gray border rounded-xl">
+                    <h4 className="m-2">Scorecard</h4>
+                    <hr />
                     <table className="w-full">
                       <thead className="border-n-bg-gray border-b">
                         <tr>
                           <th className="h-7 text-left px-1 w-2/3 sm:w-[300px] text-xxs uppercase">
-                            NAME
+                            Team
                           </th>
                           <th className=" h-7 text-center px-1 text-xxs uppercase">
                             1
@@ -231,68 +183,72 @@ const TennisDetails = () => {
                             4
                           </th>
                           <th className=" h-7 text-center px-1 text-xxs uppercase">
-                            5
+                            O
                           </th>
 
                           <th className=" h-7 text-center px-1 text-xxs uppercase">
-                            SETS
+                            T
                           </th>
                         </tr>
                       </thead>
                       <tbody>
                         <tr className="py-1 h-8 border-n-bg-gray border-b">
-                          <td className="text-left px-1 w-2/3 sm:w-[300px] text-sm capitalize flex flex-col">
-                            {game.home_player}
+                          <td
+                            className="text-left px-1 w-2/3 sm:w-[300px] text-xxs capitalize flex flex-col"
+                            style={{ fontSize: "0.8rem" }}
+                          >
+                            {game.teams.home.name}
                           </td>
-                          <td className=" text-center px-1 text-xs">
-                            {game.result && game.result.home_set1}
+                          <td className=" text-center px-1 text-xxs">
+                            {game.scores.home.quarter_1}
                           </td>
-                          <td className=" text-center px-1 text-xs">
-                            {game.result && game.result.home_set2}
+                          <td className=" text-center px-1 text-xxs">
+                            {game.scores.home.quarter_2}
                           </td>
-                          <td className=" text-center px-1 text-xs">
-                            {game.result && game.result.home_set3}
+                          <td className=" text-center px-1 text-xxs">
+                            {game.scores.home.quarter_3}
                           </td>
-                          <td className=" text-center px-1 text-xs">
-                            {game.result && game.result.home_set4}
+                          <td className=" text-center px-1 text-xxs">
+                            {game.scores.home.quarter_4}
                           </td>
-                          <td className=" text-center px-1 text-xs">
-                            {game.result && game.result.home_set5}
+                          <td className=" text-center px-1 text-xxs">
+                            {game.scores.home.over_time}
                           </td>
-
-                          <td className=" text-center px-1 text-xs">
-                            {game.result && game.result.home_sets}
+                          <td className=" text-center px-1 text-xxs">
+                            {game.scores.home.total}
                           </td>
                         </tr>
                         <tr className="py-1 h-8 border-n-bg-gray border-b">
-                          <td className="text-left px-1 w-2/3 sm:w-[300px] text-sm capitalize flex flex-col">
-                            {game.away_player}
+                          <td
+                            className="text-left px-1 w-2/3 sm:w-[300px] text-xxs capitalize flex flex-col"
+                            style={{ fontSize: "0.8rem" }}
+                          >
+                            {game.teams.away.name}
                           </td>
-                          <td className=" text-center px-1 text-xs">
-                            {game.result && game.result.away_set1}
+                          <td className=" text-center px-1 text-xxs">
+                            {game.scores.away.quarter_1}
                           </td>
-                          <td className=" text-center px-1 text-xs">
-                            {game.result && game.result.away_set2}
+                          <td className=" text-center px-1 text-xxs">
+                            {game.scores.away.quarter_2}
                           </td>
-                          <td className=" text-center px-1 text-xs">
-                            {game.result && game.result.away_set3}
+                          <td className=" text-center px-1 text-xxs">
+                            {game.scores.away.quarter_3}
                           </td>
-                          <td className=" text-center px-1 text-xs">
-                            {game.result && game.result.away_set4}
+                          <td className=" text-center px-1 text-xxs">
+                            {game.scores.away.quarter_4}
                           </td>
-                          <td className=" text-center px-1 text-xs">
-                            {game.result && game.result.away_set5}
+                          <td className=" text-center px-1 text-xxs">
+                            {game.scores.away.over_time}
                           </td>
-
-                          <td className=" text-center px-1 text-xs">
-                            {game.result && game.result.away_sets}
+                          <td className=" text-center px-1 text-xxs">
+                            {game.scores.away.total}
                           </td>
                         </tr>
                       </tbody>
                     </table>
                   </div>
                 </div>
-              )}
+              )} */}
               <div className="my-2.5">
                 <Adsense />
               </div>
