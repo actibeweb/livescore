6cff21dd87912470daebbb663553382379f31581
abhinavsiwal <abhinavsiwal@gmail.com>
2022-07-22 00:44:34 +0530

    api changes

--- src/api/football.js
+++ src/api/football.js
@@ -41,3 +41,27 @@ export const getFixtureById = async (id) => {
     throw new err();
   }
 };
+export const getCustomFixture= async (formData) => {
+  try {
+    const { data } = await axios.post(
+      `${process.env.REACT_APP_API_URL}/match/getByFilter/baseball`,
+      formData
+    );
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);     
+    throw new err();
+  }
+}
+
+export const getCustomFixtureById = async (id) => {
+  try {
+    const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/match/get/${id}`);
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+    throw new err();
+  }
+}
